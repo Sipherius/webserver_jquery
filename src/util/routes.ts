5b59c6d4459340fd0bcbf1cd6e25be0082a3beec
Sipherius <p.remscheid@yahoo.de>
2024-09-14 07:49:31 +0200

    Changed test output

--- src/util/routes.ts
+++ src/util/routes.ts
@@ -3,7 +3,7 @@ import { Express, Request, Response } from 'express';
 const Routes = {
   setup(app: Express) {
     app.get('/', (req: Request, res: Response) => {
-      res.send('<h1>Hallo Welt! :)</h1>');
+      res.send('<h1>Hello World! :)</h1>');
     });
   },
 };
